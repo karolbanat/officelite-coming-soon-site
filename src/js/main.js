8a0f8d66727be609d1d9023be162180a93e87fee
Karol Banat <karol.banat@gmail.com>
2022-07-18 16:56:44 +0200

    Basic form validation

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,14 @@ const releaseHoursLeft = document.querySelector('#release-hours');
 const releaseMinutesLeft = document.querySelector('#release-minutes');
 const releaseSecondsLeft = document.querySelector('#release-seconds');
 
+const subscriptionForm = document.querySelector('.subscription-form');
+const nameInput = subscriptionForm.querySelector('#name');
+const emailInput = subscriptionForm.querySelector('#email');
+const submitBtn = subscriptionForm.querySelector('#submit-btn');
+
 const releaseDate = moment().add(30, 'days').startOf('day');
+const emailRegex =
+	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const main = () => {
 	loadReleaseDate();
@@ -14,6 +21,7 @@ const main = () => {
 	setInterval(handleCountdown, 1000);
 };
 
+// countdowns
 const loadReleaseDate = () => {
 	if (releaseDateLabel) {
 		releaseDateLabel.setAttribute('datetime', releaseDate.format('YYYY-MM-DD'));
@@ -26,8 +34,11 @@ const handleCountdown = () => {
 	const timeLeft = moment.duration(releaseDate - now);
 
 	if (releaseDaysLeft) releaseDaysLeft.innerText = formatTime(timeLeft.days());
+
 	if (releaseHoursLeft) releaseHoursLeft.innerText = formatTime(timeLeft.hours());
+
 	if (releaseMinutesLeft) releaseMinutesLeft.innerText = formatTime(timeLeft.minutes());
+
 	if (releaseSecondsLeft) releaseSecondsLeft.innerText = formatTime(timeLeft.seconds());
 };
 
@@ -35,4 +46,29 @@ const formatTime = (time) => {
 	return `${time < 10 ? 0 : ''}${time}`;
 };
 
+// form validation
+const handleSubmitBtn = (e) => {
+	e.preventDefault();
+	validateName();
+	validateEmail();
+};
+
+const validateName = () => {
+	nameInput.classList.toggle('error', !nameInput.value);
+};
+
+const validateEmail = () => {
+	emailInput.classList.toggle('error', !emailInput.value || !email.value.match(emailRegex));
+};
+
+const removeError = (e) => {
+	e.target.classList.remove('error');
+};
+
+// event listeners
 document.addEventListener('DOMContentLoaded', main);
+submitBtn.addEventListener('click', handleSubmitBtn);
+nameInput.addEventListener('focus', (e) => removeError);
+emailInput.addEventListener('focus', (e) => removeError);
+nameInput.addEventListener('focusout', validateName);
+emailInput.addEventListener('focusout', validateEmail);
