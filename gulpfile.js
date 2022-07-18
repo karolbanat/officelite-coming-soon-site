1c549ef5720ffd468b8c6db093eff36a9214efbb
Karol Banat <karol.banat@gmail.com>
2022-07-18 13:55:17 +0200

    Adding webpack to gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,10 @@ const sourcemaps = require('gulp-sourcemaps');
 const clean = require('gulp-clean');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
+const webpack = require('webpack-stream');
+const named = require('vinyl-named');
+const through = require('through2');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 const paths = {
 	sass: './src/sass/**/*.scss',
@@ -36,12 +40,46 @@ function sassCompiler(done) {
 
 function javaScript(done) {
 	src(paths.js)
-		.pipe(sourcemaps.init())
+		.pipe(named())
 		.pipe(
-			babel({
-				presets: ['@babel/preset-env'],
+			webpack({
+				mode: 'development',
+				output: {
+					filename: 'bundle.js',
+				},
+				module: {
+					rules: [
+						{
+							use: {
+								loader: 'babel-loader',
+								options: {
+									presets: ['@babel/preset-env'],
+								},
+							},
+							test: /\.js$/,
+							exclude: /node_modules/,
+						},
+					],
+				},
+				devtool: 'inline-source-map',
+				plugins: [new MomentLocalesPlugin()],
 			})
 		)
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(
+			through.obj(function (file, enc, cb) {
+				// Dont pipe through any source map files as it will be handled
+				// by gulp-sourcemaps
+				const isSourceMap = /\.map$/.test(file.path);
+				if (!isSourceMap) this.push(file);
+				cb();
+			})
+		)
+		// .pipe(
+		// 	babel({
+		// 		presets: ['@babel/preset-env'],
+		// 	})
+		// )
 		.pipe(uglify())
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(sourcemaps.write())
