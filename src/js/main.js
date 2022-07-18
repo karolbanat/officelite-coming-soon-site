076a86dc5882bdcf52a02bf3a009a6ebc17bfb40
Karol Banat <karol.banat@gmail.com>
2022-07-18 15:31:14 +0200

    Countdown handling

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,38 @@
 import moment from 'moment';
 
-let releaseDateLabel = document.querySelector('#release-date');
+const releaseDateLabel = document.querySelector('#release-date');
+const releaseDaysLeft = document.querySelector('#release-days');
+const releaseHoursLeft = document.querySelector('#release-hours');
+const releaseMinutesLeft = document.querySelector('#release-minutes');
+const releaseSecondsLeft = document.querySelector('#release-seconds');
+
+const releaseDate = moment().add(30, 'days').startOf('day');
 
 const main = () => {
 	loadReleaseDate();
+	handleCountdown();
+	setInterval(handleCountdown, 1000);
 };
 
 const loadReleaseDate = () => {
 	if (releaseDateLabel) {
-		const release = moment().add(30, 'days').startOf('day');
-		releaseDateLabel.setAttribute('datetime', release.format('YYYY-MM-DD'));
-		releaseDateLabel.innerText = release.format('D MMM YYYY');
+		releaseDateLabel.setAttribute('datetime', releaseDate.format('YYYY-MM-DD'));
+		releaseDateLabel.innerText = releaseDate.format('D MMM YYYY');
 	}
+};
+
+const handleCountdown = () => {
+	const now = moment();
+	const timeLeft = moment.duration(releaseDate - now);
+
+	if (releaseDaysLeft) releaseDaysLeft.innerText = formatTime(timeLeft.days());
+	if (releaseHoursLeft) releaseHoursLeft.innerText = formatTime(timeLeft.hours());
+	if (releaseMinutesLeft) releaseMinutesLeft.innerText = formatTime(timeLeft.minutes());
+	if (releaseSecondsLeft) releaseSecondsLeft.innerText = formatTime(timeLeft.seconds());
+};
+
+const formatTime = (time) => {
+	return `${time < 10 ? 0 : ''}${time}`;
 };
 
 document.addEventListener('DOMContentLoaded', main);
